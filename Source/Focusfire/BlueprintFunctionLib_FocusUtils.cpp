952e23cb30ada907932f99fe944346bf2d37b988
aeqin <34169563+aeqin@users.noreply.github.com>
2025-03-11 20:19:11 -0400

    Ping Part 4 (Offscreen calculations)
    * Added a blueprint function library called FocusfireUtils, includes functions that can be used to calculate if a FocusMarker widget is on screen, and if off screen, then can calculate the angle to it as well as position an indicator showing where it is off screen

---
+++ Source/Focusfire/BlueprintFunctionLib_FocusUtils.cpp
@@ -0,0 +1,127 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlueprintFunctionLib_FocusUtils.h"
+
+#include "Components/WidgetComponent.h"
+
+bool UBlueprintFunctionLib_FocusUtils::IsWidgetComponentOnPlayerScreen(const APlayerController* ControllerRef, const UWidgetComponent* WidgetComponent)
+{
+	FVector2D _ScreenLocation;
+	int _ViewportSizeX;
+	int _ViewportSizeY;
+	
+	if (not ControllerRef->ProjectWorldLocationToScreen(WidgetComponent->GetComponentLocation(), _ScreenLocation))
+		return false; // Failed to project to screen
+	
+	ControllerRef->GetViewportSize(_ViewportSizeX, _ViewportSizeY);
+
+	if (_ScreenLocation.X < 0 || _ScreenLocation.X > _ViewportSizeX) return false; // Off left or right edge of screen
+
+	if (_ScreenLocation.Y < 0 || _ScreenLocation.Y > _ViewportSizeY) return false; // Off top or bottom edge of screen
+	
+	return true; // On screen
+}
+
+float UBlueprintFunctionLib_FocusUtils::GetPlayerToWidgetComponentAngle(const APlayerController* ControllerRef,
+	const UWidgetComponent* WidgetComponent)
+{
+	FVector _PlayerLocation = ControllerRef->GetPawn()->GetActorLocation();
+	FVector _WidgetLocation = WidgetComponent->GetComponentLocation();
+	FRotator _LookAtRotator = (_PlayerLocation - _WidgetLocation).Rotation();
+	float _AngleFromPlayerViewToWidgetComponent = _LookAtRotator.Yaw - ControllerRef->GetControlRotation().Yaw;
+	
+	return _AngleFromPlayerViewToWidgetComponent;
+}
+
+FVector2D UBlueprintFunctionLib_FocusUtils::GetViewportBoundsIntersectionFromAngle(const FVector2D ViewportBounds,
+	const float Angle)
+{
+	// Center point of the viewport
+	FVector2D _CenterPoint = ViewportBounds / 2.0f;
+
+	// Max line length would be the distance from center of rectangle to corner
+	float _MaxLineLength = (FVector2D(_CenterPoint.X, 0) - FVector2D(0, _CenterPoint.Y)).Length();
+
+	// Calculate the line angle from the GetPlayerToWidgetComponentAngle() angle
+	float _LineAngle;
+	if (Angle <= 0)
+	{
+		_LineAngle = 180 - FMath::Abs(Angle);
+	}
+	else
+	{
+		_LineAngle = Angle + 180;
+	}
+
+	// Calculate end point of line from angle (using unit vector pointing up)
+	FVector2D _LineEndPoint = _CenterPoint + FVector2D(0, -1).GetRotated(_LineAngle) * _MaxLineLength;
+
+	// Calculate intersection of line with rectangle bounds
+	FVector2D _IntersectionPoint;
+
+	// Check intersection with left of viewport
+	if (GetIntersectionFromTwoLineSegments(
+		FVector2D(0, 0), FVector2D(0, ViewportBounds.Y),
+		_CenterPoint, _LineEndPoint,
+		_IntersectionPoint))
+	{
+		return _IntersectionPoint;
+	}
+
+	// Check intersection with right of viewport
+	if (GetIntersectionFromTwoLineSegments(
+		FVector2D(ViewportBounds.X, 0), FVector2D(ViewportBounds.X, ViewportBounds.Y),
+		_CenterPoint, _LineEndPoint,
+		_IntersectionPoint))
+	{
+		return _IntersectionPoint;
+	}
+
+	// Check intersection with top of viewport
+	if (GetIntersectionFromTwoLineSegments(
+		FVector2D(0, 0), FVector2D(ViewportBounds.X, 0),
+		_CenterPoint, _LineEndPoint,
+		_IntersectionPoint))
+	{
+		return _IntersectionPoint;
+	}
+
+	// Check intersection with bot of viewport
+	if (GetIntersectionFromTwoLineSegments(
+		FVector2D(0, ViewportBounds.Y), FVector2D(ViewportBounds.X, ViewportBounds.Y),
+		_CenterPoint, _LineEndPoint,
+		_IntersectionPoint))
+	{
+		return _IntersectionPoint;
+	}
+	
+	return _IntersectionPoint;
+}
+
+bool UBlueprintFunctionLib_FocusUtils::GetIntersectionFromTwoLineSegments(const FVector2D A_Start,
+	const FVector2D A_End, const FVector2D B_Start, const FVector2D B_End, FVector2D& IntersectionPoint)
+{
+	// Calculate rise and run of each line segment
+	float _rise_A = A_End.Y - A_Start.Y;
+	float _run_A = A_End.X - A_Start.X;
+	float _rise_B = B_End.Y - B_Start.Y;
+	float _run_B = B_End.X - B_Start.X;
+
+	// Calculate alpha / beta, which is a [0, 1] "percentage" of points on a line
+	float _alpha = (-_rise_A * (A_Start.X - B_Start.X) + _run_A * (A_Start.Y - B_Start.Y)) / (-_run_B * _rise_A + _run_A * _rise_B);
+	float _beta = (_run_B * (A_Start.Y - B_Start.Y) - _rise_B * (A_Start.X - B_Start.X)) / (-_run_B * _rise_A + _run_A * _rise_B);
+
+	// There is a collision, so set intersection point
+	if (_alpha >= 0 && _alpha <= 1 && _beta >= 0 && _beta <= 1)
+	{
+		IntersectionPoint = FVector2D(A_Start.X + (_beta * _run_A), A_Start.Y + (_beta * _rise_A));
+	
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
---
+++ Source/Focusfire/BlueprintFunctionLib_FocusUtils.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BlueprintFunctionLib_FocusUtils.generated.h"
+
+class UWidgetComponent;
+/**
+ * 
+ */
+UCLASS()
+class FOCUSFIRE_API UBlueprintFunctionLib_FocusUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/**
+	 * @param ControllerRef: The reference to the controller (used to get viewport)
+	 * @param WidgetComponent: The reference to the WidgetComponent (that has a world space)
+	 * @returns: Whether the WidgetComponent is on the particular Player's screen
+	 */
+	UFUNCTION(BlueprintPure, Category = "FocusUtils")
+	static bool IsWidgetComponentOnPlayerScreen(const APlayerController* ControllerRef, const UWidgetComponent* WidgetComponent);
+
+	/**
+	 * @param ControllerRef: The reference to the controller (used to get viewport)
+	 * @param WidgetComponent: The reference to the WidgetComponent (that has a world space)
+	 * @returns: The angle (Yaw) from the Player to the WidgetComponent in world space
+	 */
+	UFUNCTION(BlueprintPure, Category = "FocusUtils")
+	static float GetPlayerToWidgetComponentAngle(const APlayerController* ControllerRef, const UWidgetComponent* WidgetComponent);
+
+	/**
+	 * @param ViewportBounds: The bounds of the viewport rectangle (assumes that (0, 0) is top left corner) (will be used to calculate the center point)
+	 * @param Angle: The angle of the line that is drawn from the center of the rectangle to one of the bounds (specifically the angle from GetPlayerToWidgetComponentAngle())
+	 * @returns: The point on a rectangle's bounds that intersects with a line from the center of the rectangle, drawn by given angle
+	 */
+	UFUNCTION(BlueprintPure, Category = "FocusUtils")
+	static FVector2D GetViewportBoundsIntersectionFromAngle(const FVector2D ViewportBounds, const float Angle);
+
+	/**
+	 * @param A_Start: The start of the first line segment
+	 * @param A_End: The end of the first line segment
+	 * @param B_Start: The start of the second line segment
+	 * @param B_End: The end of the second line segment
+	 * @param IntersectionPoint: The intersection point between the two line segments
+	 * @returns: Whether an intersection exists between the two line segments
+	 */
+	UFUNCTION(BLueprintPure, Category = "FocusUtils")
+	static bool GetIntersectionFromTwoLineSegments(const FVector2D A_Start, const FVector2D A_End, const FVector2D B_Start, const FVector2D B_End, FVector2D& IntersectionPoint);
+};
